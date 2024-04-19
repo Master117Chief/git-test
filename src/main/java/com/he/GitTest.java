c5de6360554a2adc1ce43387757550fd92fa174e
he <2301298070@qq.com>
2024-04-19 22:22:29 +0800

    hot-fix commit2

--- src/main/java/com/he/GitTest.java
+++ src/main/java/com/he/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
         System.out.println("Hallo git!1");
         System.out.println("Hallo git!2");
         System.out.println("Hallo git!3");
-
+        System.out.println("Hallo git!4");
+        System.out.println("Hot-fix test");
     }
 }
