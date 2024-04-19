295296559a5e293cf602a426541823bb4e732bd1
he <2301298070@qq.com>
2024-04-19 20:48:41 +0800

    First hot-fix

--- src/main/java/com/he/GitTest.java
+++ src/main/java/com/he/GitTest.java
@@ -2,6 +2,10 @@ package com.he;
 
 public class GitTest {
     public static void main(String[] args){
+
         System.out.println("Hallo git!");
+        System.out.println("Hallo git!2");
+        System.out.println("Hallo git!3");
+        System.out.println("Hallo git!4");
     }
 }
